fed6cf7c42410a984e07e0f75acefd7e26fc8eff
hichem94 <hichem.eld@gmail.com>
2024-08-26 12:01:07 +0200

    Fonctionne avec l'envoi d'objets

--- client.py
+++ client.py
@@ -8,17 +8,6 @@ win    = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
 
-clientNumber = 0
-
-
-
-def read_pos(s):
-    s = s.split(",")
-    return int(s[0]), int(s[1])
-
-def make_pos(tup):
-    return str(str(tup[0]) + "," + str(tup[1]))
-
 def redrawWindow(win, player1, player2):
     win.fill((255,255,255))
     player1.draw(win)
@@ -29,18 +18,13 @@ def redrawWindow(win, player1, player2):
 def main():
     run = True
     n = Network()
-    startPos = read_pos(n.getPos())
-    p1 = Player(startPos[0],startPos[1],100,100,(0,255,0))
-    p2 = Player(0,0,100,100,(0,0,255))
+    p1 = n.getP() # 0 or 1
     clock = pygame.time.Clock()
     
     while run:
         clock.tick(60)
-        p2Pos = read_pos( n.send( make_pos( (p1.x, p1.y) ) ) )
-        p2.x = p2Pos[0]
-        p2.y = p2Pos[1]
-        p2.update()
-        
+        p2 = n.send(p1)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
--- server.py
+++ server.py
@@ -1,61 +1,61 @@
 import socket
 from _thread import *
 import sys
+from player import Player
+import pickle
 
-server = "172.19.196.199" #wsl hostname -I
-port = 5555
+server = "172.19.196.199"
+port   = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Bind (server,port) to the socket
 try:
-    s.bind((server, port))
+    s.bind( (server, port) )
 except socket.error as e:
     str(e)
 
-s.listen(2)
-print("Waiting for a connection, Server Started")
-
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
+# Listening for connections
+s.listen(2) # I only want two people to be able to connect to my server
+print("Waiting for connection, Server Started")
 
 
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-pos = [(0,0),(100,100)]
+players = [Player(0,0,50,50,(255,0,0)), Player(100,100,50,50,(0,0,255))]
 
 def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
+    conn.send( pickle.dumps(players[player]) )
     reply = ""
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+            data = pickle.loads(conn.recv(2048)) # amount of informations we want to recv
+            players[player] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 else:
-                    reply = pos[1]
-
+                    reply = players[1]
+                
                 print("Received: ", data)
                 print("Sending : ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall( pickle.dumps(reply) )
         except:
             break
-
+        
     print("Lost connection")
     conn.close()
 
+
 currentPlayer = 0
 while True:
-    conn, addr = s.accept()
+    conn, addr = s.accept() # accept any incomming connections
     print("Connected to:", addr)
 
     start_new_thread(threaded_client, (conn, currentPlayer))
     currentPlayer += 1
+
+
